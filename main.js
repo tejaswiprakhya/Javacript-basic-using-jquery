e9ba8c37072957f66e85f7640923be146ff6390d
tejaswiprakhya <tejaswi.prakhya@gmail.com>
2017-08-23 22:10:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//var skillset =  document.getElementsByClassName('skillset');
+//alert(skillset);
+function main()
+{
+ 
+  $('.skillset').hide();
+  $('.skillset').fadeIn(1000);
+  $('.projects').hide();
+  $('.projects-button').on('click',function(){
+     $(this).next().slideToggle(400);
+     $(this).toggleClass('active');
+    $(this).text('Projects Viewed')
+  });
+  
+}
+$(document).ready(main);
